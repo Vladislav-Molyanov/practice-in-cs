aafe99add517126d6e961ffadfdba407377fbec2
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-01 00:48:59 +0400

    solve problem number three

--- 001_HomeWorke/Program.cs
+++ 001_HomeWorke/Program.cs
@@ -46,3 +46,21 @@ else
 
 
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int result = num % 2;
+if(result == 0) 
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
+
+
+

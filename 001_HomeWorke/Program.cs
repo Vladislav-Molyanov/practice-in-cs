8b2cc4738e05582ea8ed952930951b122bb2b590
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-01 00:47:39 +0400

    solve problem number two

--- 001_HomeWorke/Program.cs
+++ 001_HomeWorke/Program.cs
@@ -17,3 +17,32 @@ else
     Console.WriteLine($"max = {numTwo}");
 }
 
+
+// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите первое число");
+int numOne = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int numThree = int.Parse(Console.ReadLine());
+
+if (numOne > numTwo ) 
+{
+    Console.WriteLine($"max = {numOne}");
+}
+else if (numTwo > numThree)
+{
+    Console.WriteLine($"max = {numTwo}");
+}
+else 
+{
+    Console.WriteLine($"max = {numThree}");
+}
+
+
+

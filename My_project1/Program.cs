69c268376e12bd0e25b890878909f9c2f579023f
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-01 00:16:06 +0400

    practice cs_1

--- My_project1/Program.cs
+++ My_project1/Program.cs
@@ -23,42 +23,54 @@
 
 // №2
 
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number = int.Parse(Console.ReadLine());
 
-if (number == 1)
-{
-    Console.WriteLine("Понедельник");
-}
-else if(number == 2) 
-{
-    Console.WriteLine("Вторник");
-}
-else if(number == 3) 
-{
-    Console.WriteLine("Среда");
-}
-else if(number == 4) 
-{
-    Console.WriteLine("Четверг");
-}
-else if(number == 5) 
-{
-    Console.WriteLine("Пятница");
-}
-else if(number == 6) 
-{
-    Console.WriteLine("Суббота");
-}
-else if(number == 7) 
-{
-    Console.WriteLine("Воскресенье");
-}
-else 
-{
-    Console.WriteLine("Это число не соотвествует, введите число от 1 до 7");
-}
+// if (number == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if(number == 2) 
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if(number == 3) 
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if(number == 4) 
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if(number == 5) 
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if(number == 6) 
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if(number == 7) 
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else 
+// {
+//     Console.WriteLine("Это число не соотвествует, введите число от 1 до 7");
+// }
 
+//№3
+
+Console.WriteLine("Введите число n");
+int num = int.Parse(Console.ReadLine());
+
+int i = Math.Abs(num) * -1;
+
+while (i <= num) 
+{
+    Console.Write($"{i},");
+    i++;
+}
 
 
 

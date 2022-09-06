dc640ab5e09e4a57319d55b29b4807b5c1494f3f
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-06 20:37:20 +0400

    solution of the second problem

---
+++ 002_HomeWork/02_exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите натуральное число");
+string numStr = Console.ReadLine();
+
+PrintThirdDigit(numStr);
+void PrintThirdDigit(string arg) 
+{
+    if(arg.Length < 3) 
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    else 
+    {
+       Console.WriteLine(arg[2]);
+    }
+}

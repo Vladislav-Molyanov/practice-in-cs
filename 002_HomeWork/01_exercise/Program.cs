0063914e9b4bf2ad5f55036929615315822b3efe
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-06 18:52:24 +0400

    solution of the first problem

---
+++ 002_HomeWork/01_exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+
+int result = FindSecondDigit(num);
+Console.WriteLine(result);
+
+int FindSecondDigit(int arg)
+{
+   return (arg / 10) % 10;
+    
+}
+
+
+

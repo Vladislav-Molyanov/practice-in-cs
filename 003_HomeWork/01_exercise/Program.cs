311b921f800625e600bdd96fa49aea6231890dd1
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-07 22:12:19 +0400

    checking a number for a palindrome

---
+++ 003_HomeWork/01_exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿
+bool CheckPalindrome (string arg)
+{
+    for(int i = 0; i < arg.Length / 2; i++) 
+    {
+        if(arg[i] != arg[arg.Length-1-i])
+        { 
+            return false;
+        }
+        
+    }
+    return true;
+
+
+}
+
+Console.WriteLine("Введите пятизначное число");
+int nums = int.Parse(Console.ReadLine());
+nums = Math.Abs(nums);
+
+string convNumStr = Convert.ToString(nums);
+
+if(CheckPalindrome(convNumStr))
+{
+    Console.WriteLine($"Число {convNumStr} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {convNumStr} не является палиндромом");
+}
+

74d1307e14528f6d692c8609c332fd2211a50414
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-08 22:46:07 +0400

    raising a number to a cube

---
+++ 003_HomeWork/03_exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+void ConvertNumbersToCube (double arg)
+{
+    
+    for(double i = 1;i <= arg;i++)
+    {
+        
+       Console.Write($"{(int)Math.Pow(i, 3)},");
+    }
+    
+}
+
+Console.WriteLine("Введите число (N)");
+double nums = double.Parse(Console.ReadLine());
+nums = Math.Abs(nums);
+
+ConvertNumbersToCube(nums);

abc6cc3b6a507d05c37ab3b39f027ead011237ac
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-22 01:40:43 +0400

    solved problems for the sixth homework

---
+++ 006_HomeWork/01_exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] GetArray (int size)
+{
+    var arr = new int[size];
+
+   
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int  CalcNumbersMoreZero (int [] arg)
+{
+    int count = 0;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        if( arg[i] > 0)
+        {
+            count ++;
+        }
+        
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество элемментов в  массиве");
+int num = int.Parse(Console.ReadLine());
+int[] Array = GetArray(num);
+Console.WriteLine("Массив  чисел: ["+ string.Join(",", Array) +"]");
+
+int numCount = CalcNumbersMoreZero(Array);
+Console.WriteLine($"Количество числе > 0 = {numCount}");
---
+++ 006_HomeWork/02_exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// Пример:
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффицент k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффицент k2");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффицент b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффицент b2");
+double b2 = double.Parse(Console.ReadLine());
+
+
+
+if (k1 == k2 && b1 == b2) 
+{
+    Console.WriteLine("Данные точки будут на одной плоскости");
+}
+if (k1 == k2) 
+{
+    Console.WriteLine("Данные точки не пересекаются");
+}
+else 
+{
+   double x = (b2 - b1) / (k1 - k2);
+   double y = (k1 * x) + b1; 
+    Console.WriteLine($"({x};{y})");
+}
+
